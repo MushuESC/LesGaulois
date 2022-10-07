bd783626515e878577ff6f8db6c69dc5fa0a3dfa
valverde simon <vls4035a@ens.fsi>
2022-10-07 08:15:44 +0200

    Debut du TP1

---
+++ src/personnages/Druide.java
@@ -0,0 +1,49 @@
+package personnages;
+
+import java.util.*;
+
+public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+	private int forcePotion=1;
+	
+
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+	
+	public void preparerPotion() {
+		Random random=new Random();
+		random.nextInt(effetPotionMax);
+		if (forcePotion > 7) {
+			parler("J'ai préparé une super potion de force");
+			
+		} else {
+			parler("Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force");
+		} 
+	}
+
+	public static void main(String[] args) {
+		Druide panoramix=new Druide("Panoramix",5,10);
+		panoramix.preparerPotion();
+			
+	}
+
+
+}
+
+
